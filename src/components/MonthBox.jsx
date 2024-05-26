f9ec71570148ce35f0a85a5407bcc9820e04a5cb
sadiqur057 <sadiqur057@gmail.com>
2024-05-26 17:47:02 +0600

    used conditional rendering to show current date

--- src/components/MonthBox.jsx
+++ src/components/MonthBox.jsx
@@ -2,18 +2,22 @@ import PropTypes from 'prop-types';
 
 const MonthBox = ({data}) => {
   const {monthName, days} = data || {}
+  const date = new Date().getDate()
+  console.log(date)
   const weeks = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
   return (
     <div className="bg-white p-3 rounded">
       <div className=" font-medium pb-2">{monthName}</div>
-      <div className="grid grid-cols-7 gap-2">
+      <div className="grid grid-cols-7 gap-1">
         {
           weeks.map((day,idx)=><p key={idx} className="bg-[#cac8c8] text-center py-1 rounded">{day}</p>)
         }
       </div>
-      <div className="grid grid-cols-7 gap-2 pt-3">
+      <div className="grid grid-cols-7 gap-1 pt-3">
         {
-          days.map((day,idx)=><p key={idx} className="bg-[#eeeeee] text-center py-1 rounded">{day}</p>)
+          days.map((day,idx)=><p 
+          key={idx} 
+          className={` text-center py-1 rounded ${day===date ? "bg-[#cac8c8]":"bg-[#eeeeee]"} `}>{day}</p>)
         }
       </div>
 
