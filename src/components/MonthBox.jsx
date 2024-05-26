fd1f6fd06dc7fc58459b9761113d2c060a368c18
sadiqur057 <sadiqur057@gmail.com>
2024-05-26 17:54:14 +0600

    highlighted current date

--- src/components/MonthBox.jsx
+++ src/components/MonthBox.jsx
@@ -1,23 +1,26 @@
 import PropTypes from 'prop-types';
 
-const MonthBox = ({data}) => {
-  const {monthName, days} = data || {}
-  const date = new Date().getDate()
-  console.log(date)
-  const weeks = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+const MonthBox = ({ data }) => {
+  const { monthName, days } = data || {}
+  const date = new Date()
+  console.log(monthName)
+  const currentDay = date.getDate()
+  const currentMonth = date.toLocaleString('default', { month: 'long' }); 
+  console.log(currentMonth,monthName)
+  const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
   return (
     <div className="bg-white p-3 rounded">
       <div className=" font-medium pb-2">{monthName}</div>
       <div className="grid grid-cols-7 gap-1">
         {
-          weeks.map((day,idx)=><p key={idx} className="bg-[#cac8c8] text-center py-1 rounded">{day}</p>)
+          weeks.map((day, idx) => <p key={idx} className="bg-[#cac8c8] text-center py-1 rounded">{day}</p>)
         }
       </div>
       <div className="grid grid-cols-7 gap-1 pt-3">
         {
-          days.map((day,idx)=><p 
-          key={idx} 
-          className={` text-center py-1 rounded ${day===date ? "bg-[#cac8c8]":"bg-[#eeeeee]"} `}>{day}</p>)
+          days.map((day, idx) => <p
+            key={idx}
+            className={` text-center py-1 rounded ${day === currentDay && monthName === currentMonth ? "bg-[#cac8c8]" : "bg-[#eeeeee]"} `}>{day}</p>)
         }
       </div>
 
